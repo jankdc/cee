a4be8a55e324fc390abf065c6168167f94202140
Jan Karlo Dela Cruz <jankarlo.delacruz@gmail.com>
2015-07-20 09:02:28 +0100

    Added all the opcodes (currently not implemented).

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -42,9 +42,41 @@ cee::Chip8::Chip8()
     #ifndef ADD_OP
     #define ADD_OP(n) mOps.emplace(n, std::bind(&Chip8::op##n, this));
 
-    ADD_OP(0xA000)
     ADD_OP(0x0000)
+    ADD_OP(0x000E)
+    ADD_OP(0x00EE)
+    ADD_OP(0x1000)
+    ADD_OP(0x2000)
+    ADD_OP(0x3000)
+    ADD_OP(0x4000)
+    ADD_OP(0x5000)
     ADD_OP(0x6000)
+    ADD_OP(0x7000)
+    ADD_OP(0x8000)
+    ADD_OP(0x8001)
+    ADD_OP(0x8002)
+    ADD_OP(0x8003)
+    ADD_OP(0x8004)
+    ADD_OP(0x8005)
+    ADD_OP(0x8006)
+    ADD_OP(0x8007)
+    ADD_OP(0x800E)
+    ADD_OP(0x9000)
+    ADD_OP(0xA000)
+    ADD_OP(0xB000)
+    ADD_OP(0xC000)
+    ADD_OP(0xD000)
+    ADD_OP(0xE0A1)
+    ADD_OP(0xE09E)
+    ADD_OP(0xF007)
+    ADD_OP(0xF00A)
+    ADD_OP(0xF015)
+    ADD_OP(0xF018)
+    ADD_OP(0xF01E)
+    ADD_OP(0xF029)
+    ADD_OP(0xF033)
+    ADD_OP(0xF055)
+    ADD_OP(0xF065)
 
     #undef ADD_OP
     #endif // ADD_OP
@@ -85,9 +117,19 @@ void cee::Chip8::updateCycle()
     // Decode opcode
     auto op = mOpCode & 0xF000;
 
-    // In cases when the instruction prefix is at the end of the byte
-    // e.g. 0x000E
-    if (op == 0x0000) op = mOpCode & 0x000F;
+    // In cases when there's variation of options for an instruction.
+    switch (op)
+    {
+    case 0x0000: case 0xE000: case 0xF000:
+        op = mOpCode & 0xF0FF;
+        break;
+    case 0x8000:
+        op = mOpCode & 0xF00F;
+        break;
+    default:
+        // Do nothing...
+        break;
+    }
 
     // Execute opcode
     if (mOps.count(op)) mOps[op]();
@@ -114,17 +156,222 @@ void cee::Chip8::updateKeys(std::array<uint8_t, 16> keys)
 //  \___/|_|    \____\___/|____/|_____|____/
 
 
-void cee::Chip8::op0xA000()
-{
-    // TODO
-}
-
+// Calls RCA 1802 program at address NNN.
 void cee::Chip8::op0x0000()
 {
     // TODO
 }
 
+// Clears the screen.
+void cee::Chip8::op0x000E()
+{
+    // TODO
+}
+
+// Returns from a subroutine.
+void cee::Chip8::op0x00EE()
+{
+    // TODO
+}
+
+// Jumps to address NNN.
+void cee::Chip8::op0x1000()
+{
+    // TODO
+}
+
+// Calls subroutine at NNN.
+void cee::Chip8::op0x2000()
+{
+    // TODO
+}
+
+// Skips the next instruction if VX equals NN.
+void cee::Chip8::op0x3000()
+{
+    // TODO
+}
+
+// Skips the next instruction if VX doesn't equal NN.
+void cee::Chip8::op0x4000()
+{
+    // TODO
+}
+
+// Skips the next instruction if VX equals VY.
+void cee::Chip8::op0x5000()
+{
+    // TODO
+}
+
+// Sets VX to NN.
 void cee::Chip8::op0x6000()
+{
+    // TODO
+}
+
+// Adds NN to VX.
+void cee::Chip8::op0x7000()
+{
+    // TODO
+}
+
+// Sets VX to the value of VY.
+void cee::Chip8::op0x8000()
+{
+    // TODO
+}
+
+// Sets VX to VX or VY.
+void cee::Chip8::op0x8001()
+{
+    // TODO
+}
+
+// Sets VX to VX and VY.
+void cee::Chip8::op0x8002()
+{
+    // TODO
+}
+
+// Sets VX to VX xor VY.
+void cee::Chip8::op0x8003()
+{
+    // TODO
+}
+
+// Adds VY to VX. VF is set to 1 when carry, and to 0 when isn't.
+void cee::Chip8::op0x8004()
+{
+    // TODO
+}
+
+// VY is subtracted from VX. VF is set to 0 when borrow, and 1 when isn't.
+void cee::Chip8::op0x8005()
+{
+    // TODO
+}
+
+// Shifts VX right by 1. VF is set value of the least sig bit of VX before shift.
+void cee::Chip8::op0x8006()
+{
+    // TODO
+}
+
+// Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when isn't.
+void cee::Chip8::op0x8007()
+{
+    // TODO
+}
+
+// Shifts VX left by 1. VF is set value of the most sig bit of VX before shift.
+void cee::Chip8::op0x800E()
+{
+    // TODO
+}
+
+// Skips the next instruction if VX doesn't equal VY.
+void cee::Chip8::op0x9000()
+{
+    // TODO
+}
+
+// Sets I to the address NNN.
+void cee::Chip8::op0xA000()
+{
+    // TODO
+}
+
+// Jumps to the address NNN plus V0.
+void cee::Chip8::op0xB000()
+{
+    // TODO
+}
+
+// Sets VX to a random number, masked by NN.
+void cee::Chip8::op0xC000()
+{
+    // TODO
+}
+
+// Sprites stored in memory at location in index register (I), maximum 8bits wide.
+// Wraps around the screen.
+// If when drawn, clears a pixel, register VF is set to 1 otherwise it is zero.
+// All drawing is XOR drawing (i.e. it toggles the screen pixels).
+void cee::Chip8::op0xD000()
+{
+    // TODO
+}
+
+
+// Skips the next instruction if the key stored in VX is pressed.
+void cee::Chip8::op0xE0A1()
+{
+    // TODO
+}
+
+// Skips the next instruction if the key stored in VX isn't pressed.
+void cee::Chip8::op0xE09E()
+{
+    // TODO
+}
+
+// Sets VX to the value of the delay timer.
+void cee::Chip8::op0xF007()
+{
+    // TODO
+}
+
+// A key press is awaited, and then stored in VX.
+void cee::Chip8::op0xF00A()
+{
+    // TODO
+}
+
+// Sets the delay timer to VX.
+void cee::Chip8::op0xF015()
+{
+    // TODO
+}
+
+// Sets the sound timer to VX.
+void cee::Chip8::op0xF018()
+{
+    // TODO
+}
+
+// Adds VX to I.
+void cee::Chip8::op0xF01E()
+{
+    // TODO
+}
+
+// Sets I to the location of the sprite for the character in VX.
+// Characters 0-F (in hexadecimal) are represented by a 4x5 font.
+void cee::Chip8::op0xF029()
+{
+    // TODO
+}
+
+// Stores the Binary-coded decimal representation of VX,
+// with the most significant of three digits at the address in I,
+// the middle digit at I plus 1, and the least significant digit at I plus 2.
+// (In other words, take the decimal representation of VX,
+// place the hundreds digit in memory at location in I, the tens digit at location I+1,
+// and the ones digit at location I+2.).
+void cee::Chip8::op0xF033()
+{
+    // TODO
+}
+
+// Stores V0 to VX in memory starting at address I.
+void cee::Chip8::op0xF055()
+{
+    // TODO
+}
+
+// Fills V0 to VX with values from memory starting at address I.
+void cee::Chip8::op0xF065()
 {
     // TODO
 }
--- src/chip8.hpp
+++ src/chip8.hpp
@@ -38,9 +38,58 @@ namespace cee
         std::map<uint16_t, Op>    mOps;          // Mapping of operations (Ops)
 
         // Operations based on opcode
-        void op0xA000();
-        void op0x0000();
-        void op0x6000();
+        void op0x0000(); // Calls RCA 1802 program at address NNN.
+        void op0x000E(); // Clears the screen.
+        void op0x00EE(); // Returns from a subroutine.
+        void op0x1000(); // Jumps to address NNN.
+        void op0x2000(); // Calls subroutine at NNN.
+        void op0x3000(); // Skips the next instruction if VX equals NN.
+        void op0x4000(); // Skips the next instruction if VX doesn't equal NN.
+        void op0x5000(); // Skips the next instruction if VX equals VY.
+        void op0x6000(); // Sets VX to NN.
+        void op0x7000(); // Adds NN to VX.
+        void op0x8000(); // Sets VX to the value of VY.
+        void op0x8001(); // Sets VX to VX or VY.
+        void op0x8002(); // Sets VX to VX and VY.
+        void op0x8003(); // Sets VX to VX xor VY.
+        void op0x8004(); // Adds VY to VX. VF is set to 1 when carry, and to 0 when isn't.
+        void op0x8005(); // VY is subtracted from VX. VF is set to 0 when borrow, and 1 when isn't.
+        void op0x8006(); // Shifts VX right by 1. VF is set value of the least sig bit of VX before shift.
+        void op0x8007(); // Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when isn't.
+        void op0x800E(); // Shifts VX left by 1. VF is set value of the most sig bit of VX before shift.
+        void op0x9000(); // Skips the next instruction if VX doesn't equal VY.
+        void op0xA000(); // Sets I to the address NNN.
+        void op0xB000(); // Jumps to the address NNN plus V0.
+        void op0xC000(); // Sets VX to a random number, masked by NN.
+
+        // Sprites stored in memory at location in index register (I), maximum 8bits wide.
+        // Wraps around the screen.
+        // If when drawn, clears a pixel, register VF is set to 1 otherwise it is zero.
+        // All drawing is XOR drawing (i.e. it toggles the screen pixels).
+        void op0xD000();
+
+        void op0xE0A1(); // Skips the next instruction if the key stored in VX is pressed.
+        void op0xE09E(); // Skips the next instruction if the key stored in VX isn't pressed.
+        void op0xF007(); // Sets VX to the value of the delay timer.
+        void op0xF00A(); // A key press is awaited, and then stored in VX.
+        void op0xF015(); // Sets the delay timer to VX.
+        void op0xF018(); // Sets the sound timer to VX.
+        void op0xF01E(); // Adds VX to I.
+
+        // Sets I to the location of the sprite for the character in VX.
+        // Characters 0-F (in hexadecimal) are represented by a 4x5 font.
+        void op0xF029();
+
+        // Stores the Binary-coded decimal representation of VX,
+        // with the most significant of three digits at the address in I,
+        // the middle digit at I plus 1, and the least significant digit at I plus 2.
+        // (In other words, take the decimal representation of VX,
+        // place the hundreds digit in memory at location in I, the tens digit at location I+1,
+        // and the ones digit at location I+2.).
+        void op0xF033();
+
+        void op0xF055(); // Stores V0 to VX in memory starting at address I.
+        void op0xF065(); // Fills V0 to VX with values from memory starting at address I.
     };
 }
 
